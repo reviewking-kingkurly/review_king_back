5c23548c333e7d142dd23a37a73b3b6dbecba6e2
jaeseung <jhwang90801@gmail.com>
2022-08-23 16:59:08 +0900

    Update regarding Docker

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,8 @@ RUN pip install -r requirements.txt
 
 COPY . . 
 
-EXPOSE 8000   
+EXPOSE 8000
 
-CMD ["gunicorn", "--bind", "0.0.0.0:8000", "review_king.wsgi:application"]
+ENTRYPOINT [ "bash", "-l", "/user/src/app/api_entrypoint.sh" ]
+
+# CMD ["gunicorn", "--bind", "0.0.0.0:8000", "review_king.wsgi:application"]
---
+++ api_entrypoint.sh
@@ -0,0 +1,20 @@
+echo "DEBUG=$DEBUG" > .env
+echo "SECRET_KEY=$SECRET_KEY" >> .env
+echo "ALGORITHM=$ALGORITHM" >> .env
+echo "ALLOWED_HOSTS=$ALLOWED_HOSTS" >> .env
+echo "DB_NAME=$DB_NAME" >> .env
+echo "DB_USER=$DB_USER" >> .env
+echo "DB_PASSWORD=$DB_PASSWORD" >> .env
+echo "DB_HOST=$DB_HOST" >> .env
+echo "DB_PORT=$DB_PORT" >> .env
+echo "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" >> .env
+echo "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" >> .env
+echo "AWS_STORAGE_BUCKET_NAME=$AWS_STORAGE_BUCKET_NAME" >> .env
+echo "AWS_REGION=$AWS_REGION" >> .env
+echo "AWS_IMAGE_URL=$AWS_IMAGE_URL" >> .env
+
+cat .env
+
+gunicorn --bind 0.0.0.0:8000 review_king.wsgi:application
+
+# nginx -g 'daemon off;'
